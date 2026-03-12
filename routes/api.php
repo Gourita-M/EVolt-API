0a21e05170b6d397409dc927eea3af813f0ef4a5
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-12 12:17:58 +0000

    Add authentication middleware to routes and organize station-related endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\StationController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\StatsController;
+
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::apiResource('reservations', ReservationController::class);
+Route::apiResource('reservations', ReservationController::class)->middleware('auth:sanctum');
+
+Route::get('/stations', [StationController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/stations', [StationController::class, 'store'])->middleware('auth:sanctum');
+
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
@@ -16,3 +24,10 @@ Route::post('/login',[AuthController::class,'login']);
 Route::middleware('auth:sanctum')->get('/profile', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/stations/search', [StationController::class, 'search'])->middleware('auth:sanctum');
+Route::post('/reservations/Cancel/{id}', [ReservationController::class, 'cancel'])->middleware('auth:sanctum');
+
+Route::get('/History', [SessionController::class, 'history'])->middleware('auth:sanctum');
+
+Route::get('Admin/Statistics', [StatsController::class, 'index'])->middleware('auth:sanctum');
